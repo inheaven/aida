26d6e6378d1e243395df9fe68da6b92c40b8d9b8
Anatoly Ivanov <java@inhell.ru>
2011-05-26 13:40:34 +0700

    add 5 sec quotes

--- src/db/aida-create.sql
+++ src/db/aida-create.sql
@@ -47,11 +47,27 @@ create table `current`(
  ) engine=innodb default charset=cp1251;
 
 DELIMITER $$
+
 DROP TRIGGER /*!50032 IF EXISTS */ `create_quotes`$$
+
 CREATE
     /*!50017 DEFINER = 'root'@'localhost' */
     TRIGGER `create_quotes` AFTER INSERT ON `all_trades`
     FOR EACH ROW begin
+	-- 5 sec
+	set @date_5sec = date_add(timestamp(str_to_date(new.date, '%d.%m.%Y'), date_format(new.time, '%H:%i:00')),
+		                     interval extract(second from (new.time div 5)*5) second);
+
+	if (select count(*) > 0 from `quotes_5sec` where `date` = @date_5sec and `symbol` = new.symbol) then
+	    update `quotes_5sec`  set `low` = if(`low` < new.price,`low`, new.price), `high` = if(`high` > new.price, `high`, new.price),
+	        `close` = new.price, `volume` = `volume` + new.volume where `date` = @date_5sec and `symbol` = new.symbol;
+	else
+	    insert into `quotes_5sec` (`symbol`, `date`, `open`, `low`, `high`, `close`, `volume`)
+	        values (new.symbol, @date_5sec, new.price, new.price, new.price, new.price, new.volume);
+	end if;
+
+	-- 1min
+
 	set @date = timestamp(str_to_date(new.date, '%d.%m.%Y'), date_format(new.time, '%H:%i:00'));
 
 	if (select count(*) > 0 from `quotes_1min` where `date` = @date and `symbol` = new.symbol) then
@@ -63,6 +79,7 @@ CREATE
 	end if;
     end;
 $$
+
 DELIMITER ;
 
 create table `vector_forecast`(
