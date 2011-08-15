4079de6c56012878afbaf807cb68184eb710388b
Anatoly Ivanov <java@inhell.ru>
2011-08-16 00:01:23 +0700

    aida-7: Добавить таблицу сделок

--- aida-core/src/main/java/ru/inhell/aida/entity/Order.java
+++ aida-core/src/main/java/ru/inhell/aida/entity/Order.java
@@ -5,5 +5,103 @@ package ru.inhell.aida.entity;
  *         Date: 15.08.11 23:44
  */
 public class Order {
+    private Long id;
+    private String date;
+    private String time;
+    private String symbol;
+    private String code;
+    private String type;
+    private float price;
+    private int quantity;
+    private float volume;
+    private String status;
+    private Long transactionId;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getVolume() {
+        return volume;
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
 }
--- aida-core/src/main/java/ru/inhell/aida/quik/OrderBean.java
+++ aida-core/src/main/java/ru/inhell/aida/quik/OrderBean.java
@@ -1,8 +1,23 @@
 package ru.inhell.aida.quik;
 
+import com.google.inject.Inject;
+import org.apache.ibatis.session.SqlSessionManager;
+import ru.inhell.aida.entity.Order;
+
+import java.util.List;
+
 /**
  * @author Anatoly A. Ivanov java@inhell.ru
  *         Date: 15.08.11 23:41
  */
 public class OrderBean {
+    public static final String NS = OrderBean.class.getName();
+
+    @Inject
+    private SqlSessionManager sm;
+
+    @SuppressWarnings({"unchecked"})
+    public List<Order> getOrders(String date){
+        return sm.selectList("selectOrdersByDate", date);
+    }
 }
