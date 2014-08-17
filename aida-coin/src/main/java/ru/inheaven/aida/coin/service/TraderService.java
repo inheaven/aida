2f8394aa3652c9497d6def0d80a03ba6cd4ff17b
Anatoly Ivanov <java@inhell.ru>
2014-08-17 23:37:18 +0700

    fix aof get rate

--- aida-coin/src/main/java/ru/inheaven/aida/coin/service/TraderService.java
+++ aida-coin/src/main/java/ru/inheaven/aida/coin/service/TraderService.java
@@ -412,8 +412,8 @@ public class TraderService {
     public Integer getOrderRate(){
         int index = 0;
 
-        for (int i = orderTimes.size() - 1; i >= 0; ++i){
-            if (System.currentTimeMillis() - orderTimes.get(i) > 1000*60*10){
+        for (int i = orderTimes.size(); i > 0; ++i){
+            if (System.currentTimeMillis() - orderTimes.get(i-1) > 1000*60*10){
                 break;
             }
 
