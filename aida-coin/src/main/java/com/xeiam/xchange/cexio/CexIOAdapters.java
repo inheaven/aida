751fc816b3603420a0851227a2fbe46126f6c7e8
Anatoly Ivanov <java@inhell.ru>
2014-09-05 23:26:51 +0700

    add cex.io currencies

--- aida-coin/src/main/java/com/xeiam/xchange/cexio/CexIOAdapters.java
+++ aida-coin/src/main/java/com/xeiam/xchange/cexio/CexIOAdapters.java
@@ -32,154 +32,196 @@ import com.xeiam.xchange.utils.DateUtils;
 
 public class CexIOAdapters {
 
-  /**
-   * Adapts a CexIOTrade to a Trade Object
-   * 
-   * @param trade CexIO trade object
-   * @param tradableIdentifier First currency in the pair
-   * @param currency Second currency in the pair
-   * @return The XChange Trade
-   */
-  public static Trade adaptTrade(CexIOTrade trade, CurrencyPair currencyPair) {
+    /**
+     * Adapts a CexIOTrade to a Trade Object
+     *
+     * @param trade CexIO trade object
+     * @param tradableIdentifier First currency in the pair
+     * @param currency Second currency in the pair
+     * @return The XChange Trade
+     */
+    public static Trade adaptTrade(CexIOTrade trade, CurrencyPair currencyPair) {
 
-    BigDecimal amount = trade.getAmount();
-    BigDecimal price = trade.getPrice();
-    Date date = DateUtils.fromMillisUtc(trade.getDate() * 1000L);
-    // Cex.IO API does not return trade type
-    return new Trade(null, amount, currencyPair, price, date, String.valueOf(trade.getTid()));
-  }
-
-  /**
-   * Adapts a CexIOTrade[] to a Trades Object
-   * 
-   * @param cexioTrades The CexIO trade data returned by API
-   * @param tradableIdentifier First currency of the pair
-   * @param currency Second currency of the pair
-   * @return The trades
-   */
-  public static Trades adaptTrades(CexIOTrade[] cexioTrades, CurrencyPair currencyPair) {
-
-    List<Trade> tradesList = new ArrayList<Trade>();
-    long lastTradeId = 0;
-    for (CexIOTrade trade : cexioTrades) {
-      long tradeId = trade.getTid();
-      if (tradeId > lastTradeId)
-        lastTradeId = tradeId;
-      // Date is reversed order. Insert at index 0 instead of appending
-      tradesList.add(0, adaptTrade(trade, currencyPair));
-    }
-    return new Trades(tradesList, lastTradeId, TradeSortType.SortByID);
-  }
-
-  /**
-   * Adapts a CexIOTicker to a Ticker Object
-   * 
-   * @param ticker The exchange specific ticker
-   * @param tradableIdentifier The tradeable identifier (e.g. BTC in BTC/USD)
-   * @param currency The currency (e.g. USD in BTC/USD)
-   * @return The ticker
-   */
-  public static Ticker adaptTicker(CexIOTicker ticker, CurrencyPair currencyPair) {
-
-    BigDecimal last = ticker.getLast();
-    BigDecimal bid = ticker.getBid();
-    BigDecimal ask = ticker.getAsk();
-    BigDecimal high = ticker.getHigh();
-    BigDecimal low = ticker.getLow();
-    BigDecimal volume = ticker.getVolume();
-    Date timestamp = new Date(ticker.getTimestamp() * 1000L);
-
-    return Ticker.TickerBuilder.newInstance().withCurrencyPair(currencyPair).withLast(last).withBid(bid).withAsk(ask).withHigh(high).withLow(low).withVolume(volume).withTimestamp(timestamp).build();
-  }
-
-  /**
-   * Adapts Cex.IO Depth to OrderBook Object
-   * 
-   * @param depth Cex.IO order book
-   * @param tradableIdentifier The tradable identifier (e.g. BTC in BTC/USD)
-   * @param currency The currency (e.g. USD in BTC/USD)
-   * @return The XChange OrderBook
-   */
-  public static OrderBook adaptOrderBook(CexIODepth depth, CurrencyPair currencyPair) {
-
-    List<LimitOrder> asks = createOrders(currencyPair, Order.OrderType.ASK, depth.getAsks());
-    List<LimitOrder> bids = createOrders(currencyPair, Order.OrderType.BID, depth.getBids());
-    Date date = new Date(depth.getTimestamp() * 1000);
-    return new OrderBook(date, asks, bids);
-  }
-
-  /**
-   * Adapts CexIOBalanceInfo to AccountInfo
-   * 
-   * @param balance CexIOBalanceInfo balance
-   * @param userName The user name
-   * @return The account info
-   */
-  public static AccountInfo adaptAccountInfo(CexIOBalanceInfo balance, String userName) {
-
-    List<Wallet> wallets = new ArrayList<Wallet>();
-
-    // Adapt to XChange DTOs
-    if (balance.getBalanceBTC() != null) {
-      wallets.add(new Wallet(Currencies.BTC, balance.getBalanceBTC().getAvailable(), "available"));
-      wallets.add(new Wallet(Currencies.BTC, balance.getBalanceBTC().getOrders(), "orders"));
-    }
-    if (balance.getBalanceLTC() != null) {
-      wallets.add(new Wallet(Currencies.LTC, balance.getBalanceLTC().getAvailable(), "available"));
-      wallets.add(new Wallet(Currencies.LTC, balance.getBalanceLTC().getOrders(), "orders"));
-    }
-    if (balance.getBalanceNMC() != null) {
-      wallets.add(new Wallet(Currencies.NMC, balance.getBalanceNMC().getAvailable(), "available"));
-      wallets.add(new Wallet(Currencies.NMC, balance.getBalanceNMC().getOrders(), "orders"));
-    }
-    if (balance.getBalanceIXC() != null) {
-      wallets.add(new Wallet(Currencies.IXC, balance.getBalanceIXC().getAvailable(), "available"));
-    }
-    if (balance.getBalanceDVC() != null) {
-      wallets.add(new Wallet(Currencies.DVC, balance.getBalanceDVC().getAvailable(), "available"));
-    }
-    if (balance.getBalanceGHS() != null) {
-      wallets.add(new Wallet(Currencies.GHs, balance.getBalanceGHS().getAvailable(), "available"));
-      wallets.add(new Wallet(Currencies.GHs, balance.getBalanceGHS().getOrders(), "orders"));
-    }
-    return new AccountInfo(userName, null, wallets);
-  }
-
-  public static List<LimitOrder> createOrders(CurrencyPair currencyPair, Order.OrderType orderType, List<List<BigDecimal>> orders) {
-
-    List<LimitOrder> limitOrders = new ArrayList<LimitOrder>();
-    for (List<BigDecimal> o : orders) {
-      checkArgument(o.size() == 2, "Expected a pair (price, amount) but got {0} elements.", o.size());
-      limitOrders.add(createOrder(currencyPair, o, orderType));
-    }
-    return limitOrders;
-  }
-
-  public static LimitOrder createOrder(CurrencyPair currencyPair, List<BigDecimal> priceAndAmount, Order.OrderType orderType) {
-
-    return new LimitOrder(orderType, priceAndAmount.get(1), currencyPair, "", null, priceAndAmount.get(0));
-  }
-
-  public static void checkArgument(boolean argument, String msgPattern, Object... msgArgs) {
-
-    if (!argument) {
-      throw new IllegalArgumentException(MessageFormat.format(msgPattern, msgArgs));
-    }
-  }
-
-  public static OpenOrders adaptOpenOrders(List<CexIOOrder> cexIOOrderList) {
-
-    List<LimitOrder> limitOrders = new ArrayList<LimitOrder>();
-
-    for (CexIOOrder cexIOOrder : cexIOOrderList) {
-      Order.OrderType orderType = cexIOOrder.getType() == CexIOOrder.Type.buy ? Order.OrderType.BID : Order.OrderType.ASK;
-      String id = Long.toString(cexIOOrder.getId());
-      limitOrders.add(new LimitOrder(orderType, cexIOOrder.getPending(), new CurrencyPair(cexIOOrder.getTradableIdentifier(), cexIOOrder.getTransactionCurrency()), id, DateUtils
-          .fromMillisUtc(cexIOOrder.getTime()), cexIOOrder.getPrice()));
+        BigDecimal amount = trade.getAmount();
+        BigDecimal price = trade.getPrice();
+        Date date = DateUtils.fromMillisUtc(trade.getDate() * 1000L);
+        // Cex.IO API does not return trade type
+        return new Trade(null, amount, currencyPair, price, date, String.valueOf(trade.getTid()));
     }
 
-    return new OpenOrders(limitOrders);
+    /**
+     * Adapts a CexIOTrade[] to a Trades Object
+     *
+     * @param cexioTrades The CexIO trade data returned by API
+     * @param tradableIdentifier First currency of the pair
+     * @param currency Second currency of the pair
+     * @return The trades
+     */
+    public static Trades adaptTrades(CexIOTrade[] cexioTrades, CurrencyPair currencyPair) {
 
-  }
+        List<Trade> tradesList = new ArrayList<Trade>();
+        long lastTradeId = 0;
+        for (CexIOTrade trade : cexioTrades) {
+            long tradeId = trade.getTid();
+            if (tradeId > lastTradeId)
+                lastTradeId = tradeId;
+            // Date is reversed order. Insert at index 0 instead of appending
+            tradesList.add(0, adaptTrade(trade, currencyPair));
+        }
+        return new Trades(tradesList, lastTradeId, TradeSortType.SortByID);
+    }
+
+    /**
+     * Adapts a CexIOTicker to a Ticker Object
+     *
+     * @param ticker The exchange specific ticker
+     * @param tradableIdentifier The tradeable identifier (e.g. BTC in BTC/USD)
+     * @param currency The currency (e.g. USD in BTC/USD)
+     * @return The ticker
+     */
+    public static Ticker adaptTicker(CexIOTicker ticker, CurrencyPair currencyPair) {
+
+        BigDecimal last = ticker.getLast();
+        BigDecimal bid = ticker.getBid();
+        BigDecimal ask = ticker.getAsk();
+        BigDecimal high = ticker.getHigh();
+        BigDecimal low = ticker.getLow();
+        BigDecimal volume = ticker.getVolume();
+        Date timestamp = new Date(ticker.getTimestamp() * 1000L);
+
+        return Ticker.TickerBuilder.newInstance().withCurrencyPair(currencyPair).withLast(last).withBid(bid).withAsk(ask).withHigh(high).withLow(low).withVolume(volume).withTimestamp(timestamp).build();
+    }
+
+    /**
+     * Adapts Cex.IO Depth to OrderBook Object
+     *
+     * @param depth Cex.IO order book
+     * @param tradableIdentifier The tradable identifier (e.g. BTC in BTC/USD)
+     * @param currency The currency (e.g. USD in BTC/USD)
+     * @return The XChange OrderBook
+     */
+    public static OrderBook adaptOrderBook(CexIODepth depth, CurrencyPair currencyPair) {
+
+        List<LimitOrder> asks = createOrders(currencyPair, Order.OrderType.ASK, depth.getAsks());
+        List<LimitOrder> bids = createOrders(currencyPair, Order.OrderType.BID, depth.getBids());
+        Date date = new Date(depth.getTimestamp() * 1000);
+        return new OrderBook(date, asks, bids);
+    }
+
+    /**
+     * Adapts CexIOBalanceInfo to AccountInfo
+     *
+     * @param balance CexIOBalanceInfo balance
+     * @param userName The user name
+     * @return The account info
+     */
+    public static AccountInfo adaptAccountInfo(CexIOBalanceInfo balance, String userName) {
+
+        List<Wallet> wallets = new ArrayList<Wallet>();
+
+        // Adapt to XChange DTOs
+        if (balance.getBalanceBTC() != null) {
+            wallets.add(new Wallet(Currencies.BTC, balance.getBalanceBTC().getAvailable(), "available"));
+            wallets.add(new Wallet(Currencies.BTC, balance.getBalanceBTC().getOrders(), "orders"));
+        }
+        if (balance.getBalanceLTC() != null) {
+            wallets.add(new Wallet(Currencies.LTC, balance.getBalanceLTC().getAvailable(), "available"));
+            wallets.add(new Wallet(Currencies.LTC, balance.getBalanceLTC().getOrders(), "orders"));
+        }
+        if (balance.getBalanceNMC() != null) {
+            wallets.add(new Wallet(Currencies.NMC, balance.getBalanceNMC().getAvailable(), "available"));
+            wallets.add(new Wallet(Currencies.NMC, balance.getBalanceNMC().getOrders(), "orders"));
+        }
+        if (balance.getBalanceIXC() != null) {
+            wallets.add(new Wallet(Currencies.IXC, balance.getBalanceIXC().getAvailable(), "available"));
+        }
+        if (balance.getBalanceDVC() != null) {
+            wallets.add(new Wallet(Currencies.DVC, balance.getBalanceDVC().getAvailable(), "available"));
+        }
+
+        if (balance.getBalanceGHS() != null) {
+            wallets.add(new Wallet(Currencies.GHs, balance.getBalanceGHS().getAvailable(), "available"));
+            wallets.add(new Wallet(Currencies.GHs, balance.getBalanceGHS().getOrders(), "orders"));
+        }
+
+        if (balance.getBalanceDRK() != null) {
+            wallets.add(new Wallet(Currencies.DRK, balance.getBalanceDRK().getAvailable(), "available"));
+            wallets.add(new Wallet(Currencies.DRK, balance.getBalanceDRK().getOrders(), "orders"));
+        }
+
+        if (balance.getBalanceDOGE() != null) {
+            wallets.add(new Wallet(Currencies.DOGE, balance.getBalanceDOGE().getAvailable(), "available"));
+            wallets.add(new Wallet(Currencies.DOGE, balance.getBalanceDOGE().getOrders(), "orders"));
+        }
+
+        if (balance.getBalanceFTC() != null) {
+            wallets.add(new Wallet(Currencies.FTC, balance.getBalanceFTC().getAvailable(), "available"));
+            wallets.add(new Wallet(Currencies.FTC, balance.getBalanceFTC().getOrders(), "orders"));
+        }
+
+        if (balance.getBalanceAUR() != null) {
+            wallets.add(new Wallet("AUR", balance.getBalanceAUR().getAvailable(), "available"));
+            wallets.add(new Wallet("AUR", balance.getBalanceAUR().getOrders(), "orders"));
+        }
+
+        if (balance.getBalancePOT() != null) {
+            wallets.add(new Wallet("POT", balance.getBalancePOT().getAvailable(), "available"));
+            wallets.add(new Wallet("POT", balance.getBalancePOT().getOrders(), "orders"));
+        }
+
+        if (balance.getBalanceANC() != null) {
+            wallets.add(new Wallet("ANC", balance.getBalanceANC().getAvailable(), "available"));
+            wallets.add(new Wallet("ANC", balance.getBalanceANC().getOrders(), "orders"));
+        }
+
+        if (balance.getBalanceMEC() != null) {
+            wallets.add(new Wallet(Currencies.MEC, balance.getBalanceMEC().getAvailable(), "available"));
+            wallets.add(new Wallet(Currencies.MEC, balance.getBalanceMEC().getOrders(), "orders"));
+        }
+
+        if (balance.getBalanceWDC() != null) {
+            wallets.add(new Wallet(Currencies.WDC, balance.getBalanceWDC().getAvailable(), "available"));
+            wallets.add(new Wallet(Currencies.WDC, balance.getBalanceWDC().getOrders(), "orders"));
+        }
+
+        return new AccountInfo(userName, null, wallets);
+    }
+
+    public static List<LimitOrder> createOrders(CurrencyPair currencyPair, Order.OrderType orderType, List<List<BigDecimal>> orders) {
+
+        List<LimitOrder> limitOrders = new ArrayList<LimitOrder>();
+        for (List<BigDecimal> o : orders) {
+            checkArgument(o.size() == 2, "Expected a pair (price, amount) but got {0} elements.", o.size());
+            limitOrders.add(createOrder(currencyPair, o, orderType));
+        }
+        return limitOrders;
+    }
+
+    public static LimitOrder createOrder(CurrencyPair currencyPair, List<BigDecimal> priceAndAmount, Order.OrderType orderType) {
+
+        return new LimitOrder(orderType, priceAndAmount.get(1), currencyPair, "", null, priceAndAmount.get(0));
+    }
+
+    public static void checkArgument(boolean argument, String msgPattern, Object... msgArgs) {
+
+        if (!argument) {
+            throw new IllegalArgumentException(MessageFormat.format(msgPattern, msgArgs));
+        }
+    }
+
+    public static OpenOrders adaptOpenOrders(List<CexIOOrder> cexIOOrderList) {
+
+        List<LimitOrder> limitOrders = new ArrayList<LimitOrder>();
+
+        for (CexIOOrder cexIOOrder : cexIOOrderList) {
+            Order.OrderType orderType = cexIOOrder.getType() == CexIOOrder.Type.buy ? Order.OrderType.BID : Order.OrderType.ASK;
+            String id = Long.toString(cexIOOrder.getId());
+            limitOrders.add(new LimitOrder(orderType, cexIOOrder.getPending(), new CurrencyPair(cexIOOrder.getTradableIdentifier(), cexIOOrder.getTransactionCurrency()), id, DateUtils
+                    .fromMillisUtc(cexIOOrder.getTime()), cexIOOrder.getPrice()));
+        }
+
+        return new OpenOrders(limitOrders);
+
+    }
 
 }
--- aida-coin/src/main/java/com/xeiam/xchange/cexio/dto/account/CexIOBalanceInfo.java
+++ aida-coin/src/main/java/com/xeiam/xchange/cexio/dto/account/CexIOBalanceInfo.java
@@ -1,8 +1,8 @@
 package com.xeiam.xchange.cexio.dto.account;
 
-import java.text.MessageFormat;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.text.MessageFormat;
 
 /**
  * Author: brox
@@ -10,93 +10,157 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public class CexIOBalanceInfo {
 
-  private final String error;
-  private final long timestamp;
-  private final String username;
-  private final CexIOBalance balanceBTC;
-  private final CexIOBalance balanceLTC;
-  private final CexIOBalance balanceNMC;
-  private final CexIOBalance balanceIXC;
-  private final CexIOBalance balanceDVC;
-  private final CexIOBalance balanceGHS;
+    private final String error;
+    private final long timestamp;
+    private final String username;
+    private final CexIOBalance balanceBTC;
+    private final CexIOBalance balanceLTC;
+    private final CexIOBalance balanceNMC;
+    private final CexIOBalance balanceIXC;
+    private final CexIOBalance balanceDVC;
+    private final CexIOBalance balanceGHS;
 
-  /**
-   * Constructor
-   * 
-   * @param error
-   * @param timestamp The server time (Unix time)
-   * @param username
-   * @param balanceBTC
-   * @param balanceNMC
-   * @param balanceIXC
-   * @param balanceDVC
-   * @param balanceGHS
-   */
-  public CexIOBalanceInfo(@JsonProperty("error") String error, @JsonProperty("timestamp") long timestamp, @JsonProperty("username") String username, @JsonProperty("BTC") CexIOBalance balanceBTC,
-      @JsonProperty("LTC") CexIOBalance balanceLTC, @JsonProperty("NMC") CexIOBalance balanceNMC, @JsonProperty("IXC") CexIOBalance balanceIXC, @JsonProperty("DVC") CexIOBalance balanceDVC,
-      @JsonProperty("GHS") CexIOBalance balanceGHS) {
+    private final CexIOBalance balanceDRK;
+    private final CexIOBalance balanceDOGE;
+    private final CexIOBalance balanceFTC;
+    private final CexIOBalance balanceAUR;
+    private final CexIOBalance balancePOT;
+    private final CexIOBalance balanceANC;
+    private final CexIOBalance balanceMEC;
+    private final CexIOBalance balanceWDC;
 
-    this.error = error;
-    this.timestamp = timestamp;
-    this.username = username;
-    this.balanceBTC = balanceBTC;
-    this.balanceLTC = balanceLTC;
-    this.balanceNMC = balanceNMC;
-    this.balanceIXC = balanceIXC;
-    this.balanceDVC = balanceDVC;
-    this.balanceGHS = balanceGHS;
-  }
 
-  public String getError() {
 
-    return error;
-  }
+    /**
+     * Constructor
+     *
+     * @param error
+     * @param timestamp The server time (Unix time)
+     * @param username
+     * @param balanceBTC
+     * @param balanceNMC
+     * @param balanceIXC
+     * @param balanceDVC
+     * @param balanceGHS
+     */
+    public CexIOBalanceInfo(@JsonProperty("error") String error, @JsonProperty("timestamp") long timestamp,
+                            @JsonProperty("username") String username, @JsonProperty("BTC") CexIOBalance balanceBTC,
+                            @JsonProperty("LTC") CexIOBalance balanceLTC, @JsonProperty("NMC") CexIOBalance balanceNMC,
+                            @JsonProperty("IXC") CexIOBalance balanceIXC, @JsonProperty("DVC") CexIOBalance balanceDVC,
+                            @JsonProperty("GHS") CexIOBalance balanceGHS,
 
-  public long getTimestamp() {
+                            @JsonProperty("DRK") CexIOBalance balanceDRK,
+                            @JsonProperty("DOGE") CexIOBalance balanceDOGE,
+                            @JsonProperty("FTC") CexIOBalance balanceFTC,
+                            @JsonProperty("AUR") CexIOBalance balanceAUR,
+                            @JsonProperty("POT") CexIOBalance balancePOT,
+                            @JsonProperty("ANC") CexIOBalance balanceANC,
+                            @JsonProperty("MEC") CexIOBalance balanceMEC,
+                            @JsonProperty("WDC") CexIOBalance balanceWDC
+                            ) {
 
-    return timestamp;
-  }
+        this.error = error;
+        this.timestamp = timestamp;
+        this.username = username;
+        this.balanceBTC = balanceBTC;
+        this.balanceLTC = balanceLTC;
+        this.balanceNMC = balanceNMC;
+        this.balanceIXC = balanceIXC;
+        this.balanceDVC = balanceDVC;
+        this.balanceGHS = balanceGHS;
 
-  public String getUsername() {
+        this.balanceDRK = balanceDRK;
+        this.balanceDOGE = balanceDOGE;
+        this.balanceFTC = balanceFTC;
+        this.balanceAUR = balanceAUR;
+        this.balancePOT = balancePOT;
+        this.balanceANC = balanceANC;
+        this.balanceMEC = balanceMEC;
+        this.balanceWDC = balanceWDC;
+    }
 
-    return username;
-  }
+    public String getError() {
 
-  public CexIOBalance getBalanceBTC() {
+        return error;
+    }
 
-    return balanceBTC;
-  }
+    public long getTimestamp() {
 
-  public CexIOBalance getBalanceLTC() {
+        return timestamp;
+    }
 
-    return balanceLTC;
-  }
+    public String getUsername() {
 
-  public CexIOBalance getBalanceNMC() {
+        return username;
+    }
 
-    return balanceNMC;
-  }
+    public CexIOBalance getBalanceBTC() {
 
-  public CexIOBalance getBalanceIXC() {
+        return balanceBTC;
+    }
 
-    return balanceIXC;
-  }
+    public CexIOBalance getBalanceLTC() {
 
-  public CexIOBalance getBalanceDVC() {
+        return balanceLTC;
+    }
 
-    return balanceDVC;
-  }
+    public CexIOBalance getBalanceNMC() {
 
-  public CexIOBalance getBalanceGHS() {
+        return balanceNMC;
+    }
 
-    return balanceGHS;
-  }
+    public CexIOBalance getBalanceIXC() {
 
-  @Override
-  public String toString() {
+        return balanceIXC;
+    }
 
-    return MessageFormat.format("CexIOBalanceInfo[error={0}, timestamp={1}, username={2}, BTC={3}, LTC={4}, NMC={5}, IXC={6}, DVC={7}, GHS={8}]", error, timestamp, username, balanceBTC, balanceLTC,
-        balanceNMC, balanceIXC, balanceDVC, balanceGHS);
-  }
+    public CexIOBalance getBalanceDVC() {
+
+        return balanceDVC;
+    }
+
+    public CexIOBalance getBalanceGHS() {
+
+        return balanceGHS;
+    }
+
+    public CexIOBalance getBalanceDRK() {
+        return balanceDRK;
+    }
+
+    public CexIOBalance getBalanceDOGE() {
+        return balanceDOGE;
+    }
+
+    public CexIOBalance getBalanceFTC() {
+        return balanceFTC;
+    }
+
+    public CexIOBalance getBalanceAUR() {
+        return balanceAUR;
+    }
+
+    public CexIOBalance getBalancePOT() {
+        return balancePOT;
+    }
+
+    public CexIOBalance getBalanceANC() {
+        return balanceANC;
+    }
+
+    public CexIOBalance getBalanceMEC() {
+        return balanceMEC;
+    }
+
+    public CexIOBalance getBalanceWDC() {
+        return balanceWDC;
+    }
+
+    @Override
+    public String toString() {
+
+        return MessageFormat.format("CexIOBalanceInfo[error={0}, timestamp={1}, username={2}, BTC={3}, LTC={4}, NMC={5}, IXC={6}, DVC={7}, GHS={8}]", error, timestamp, username, balanceBTC, balanceLTC,
+                balanceNMC, balanceIXC, balanceDVC, balanceGHS);
+    }
 
 }
