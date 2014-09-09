15c441ba262615d0ef8effc74ff459e9ca8e6ab9
Anatoly Ivanov <java@inhell.ru>
2014-09-09 08:50:42 +0700

    animate color

--- aida-coin/src/main/java/ru/inheaven/aida/coin/web/TraderList.java
+++ aida-coin/src/main/java/ru/inheaven/aida/coin/web/TraderList.java
@@ -587,7 +587,7 @@ public class TraderList extends AbstractPage{
 
                 handler.appendJavaScript(new JsStatement().$(component)
                         .chain("parent")
-                        .chain("animate", "{\"backgroundColor:\"" + color + "}")
+                        .chain("animate", "{backgroundColor:" + color + "}")
                         .render());
 
                 component.setDefaultModelObject(newValue);
