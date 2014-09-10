3e2995f1a4e62ba0c6adb530343682c227b51b89
Anatoly Ivanov <java@inhell.ru>
2014-09-10 15:01:02 +0700

    fix random min amount

--- aida-coin/src/main/java/ru/inheaven/aida/coin/web/TraderList.java
+++ aida-coin/src/main/java/ru/inheaven/aida/coin/web/TraderList.java
@@ -137,16 +137,16 @@ public class TraderList extends AbstractPage{
                         //todo extract min order config
                         switch (trader.getCounterSymbol()) {
                             case "BTC":
-                                minOrderAmount = new BigDecimal("0.0021");
+                                minOrderAmount = new BigDecimal("0.0013");
                                 break;
                             case "LTC":
-                                minOrderAmount = new BigDecimal("0.021");
+                                minOrderAmount = new BigDecimal("0.013");
                                 break;
                             case "USD":
                                 minOrderAmount = new BigDecimal("6.25");
                                 break;
                             case "CNY":
-                                minOrderAmount = new BigDecimal("21");
+                                minOrderAmount = new BigDecimal("13");
                                 break;
                         }
 
