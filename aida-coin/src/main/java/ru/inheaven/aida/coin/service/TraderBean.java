abf79070fa7db154135c8d00810cc7ec5a552588
Anatoly Ivanov <java@inhell.ru>
2014-10-23 20:49:33 +0700

    add empty traders to liquid

--- aida-coin/src/main/java/ru/inheaven/aida/coin/service/TraderBean.java
+++ aida-coin/src/main/java/ru/inheaven/aida/coin/service/TraderBean.java
@@ -26,10 +26,20 @@ public class TraderBean {
     }
 
     public List<Trader> getLiquidTraders(){
-        return em.createQuery("select distinct t from Trader t left join BalanceHistory h " +
+        List<Trader> list =  em.createQuery("select distinct t from Trader t left join BalanceHistory h " +
                 "on (h.exchangeType = t.exchange and h.pair = t.pair) where h.date > :date " +
                 "group by h.exchangeType, h.pair order by count(h.id) desc", Trader.class)
                 .setParameter("date", new Date(System.currentTimeMillis() - 1000*60*60*24)).getResultList();
+
+        List<Trader> all = getTraders();
+
+        for (Trader t : all){
+            if (!list.contains(t)){
+                list.add(t);
+            }
+        }
+
+        return list;
     }
 
     public List<Trader> getTraders(ExchangeType exchangeType){
