89a8ed17d44f1cbd0aa847ac9b90cc0234052f92
Anatoly Ivanov <java@inhell.ru>
2014-11-04 15:43:32 +0600

    set 4 P predict

--- aida-coin/src/main/java/ru/inheaven/aida/predictor/service/PredictorService.java
+++ aida-coin/src/main/java/ru/inheaven/aida/predictor/service/PredictorService.java
@@ -10,7 +10,7 @@ import javax.ejb.Singleton;
  */
 @Singleton
 public class PredictorService {
-    private VectorForecastSSA vectorForecastSSA = new VectorForecastSSA(100, 50, 5, 5);
+    private VectorForecastSSA vectorForecastSSA = new VectorForecastSSA(100, 50, 4, 5);
 
     public float getPrediction(float[] timeSeries){
         if (timeSeries.length < 100){
