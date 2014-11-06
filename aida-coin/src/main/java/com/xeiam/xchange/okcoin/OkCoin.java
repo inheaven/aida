c06464c45f75d364b4aeabec7ab792a81ea717b6
Anatoly Ivanov <java@inhell.ru>
2014-11-06 14:30:48 +0600

    fix okcoin digest

--- aida-coin/src/main/java/com/xeiam/xchange/okcoin/OkCoin.java
+++ aida-coin/src/main/java/com/xeiam/xchange/okcoin/OkCoin.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 
 @Path("/v1")
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 @Produces(MediaType.APPLICATION_JSON)
 public interface OkCoin {
 
