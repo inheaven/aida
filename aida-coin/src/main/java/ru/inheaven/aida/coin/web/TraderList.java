ccd8b61d9ef6b2d8520dc1ed08d2870b4e4145d7
Anatoly Ivanov <java@inhell.ru>
2014-11-27 02:52:52 +0600

    add order stat volume on risk chart

--- aida-coin/src/main/java/ru/inheaven/aida/coin/web/TraderList.java
+++ aida-coin/src/main/java/ru/inheaven/aida/coin/web/TraderList.java
@@ -541,7 +541,7 @@ public class TraderList extends AbstractPage{
                             Point point = new Point(s.getAvgPrice(), s.getSumAmount());
 
                             if (ticker.getLast().setScale(0, ROUND_UP).compareTo(s.getAvgPrice()) == 0){
-                                point.setColor(new HexColor("#000000"));
+                                point.setColor(new HexColor("#5C5C61"));
                             }
 
                             //noinspection unchecked
