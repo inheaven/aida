8d0a0659fb6f828340e09eff860f27c2257e47fe
Anatoly Ivanov <java@inhell.ru>
2014-12-07 22:21:15 +0600

    fix volume chart

--- aida-coin/src/main/java/ru/inheaven/aida/coin/service/TraderBean.java
+++ aida-coin/src/main/java/ru/inheaven/aida/coin/service/TraderBean.java
@@ -183,9 +183,9 @@ public class TraderBean {
 
     @SuppressWarnings("unchecked")
     public List<OrderStat> getOrderStatVolume(ExchangePair exchangePair, Date startDate){
-        return em.createNativeQuery("select id, ceil(price) as avgPrice, " +
+        return em.createNativeQuery("select id, round(price,1) as avgPrice, " +
                 "sum(FILLEDAMOUNT) as sumAmount, type, exchangeType, pair FROM order_history " +
-                "WHERE exchangetype = ? and pair = ? and status='CLOSED' and CLOSED > ? GROUP BY ceil(price)", OrderStat.class)
+                "WHERE exchangetype = ? and pair = ? and status='CLOSED' and CLOSED > ? GROUP BY round(price, 1)", OrderStat.class)
                 .setParameter(1, exchangePair.getExchangeType().name())
                 .setParameter(2, exchangePair.getPair())
                 .setParameter(3, startDate)
