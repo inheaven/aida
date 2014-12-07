9c29a1eb457361c82496132498dec989737f0453
Anatoly Ivanov <java@inhell.ru>
2014-12-07 18:30:19 +0600

    double prediction

--- aida-coin/src/main/java/ru/inheaven/aida/predictor/util/VectorForecastSSA.java
+++ aida-coin/src/main/java/ru/inheaven/aida/predictor/util/VectorForecastSSA.java
@@ -50,7 +50,7 @@ public class VectorForecastSSA {
      * @param M - длина прогноза
      */
     public VectorForecastSSA(int N, int L, int P, int M) {
-        this(N, L, P, new int[0], M, BasicAnalysisSSA.TYPE.SGESDD);
+        this(N, L, P, new int[0], M, BasicAnalysisSSA.TYPE.SGESVD);
     }
 
     public VectorForecastSSA(int N, int L, int P, int[] PP, int M){
