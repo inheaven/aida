245f53ee89bfa9a31706bc6b35232a278577399b
Anatoly Ivanov <anatoly@inheaven.ru>
2015-01-12 14:54:21 +0600

    fix short chart rebalancing

--- aida-coin/src/main/java/ru/inheaven/aida/coin/service/TraderService.java
+++ aida-coin/src/main/java/ru/inheaven/aida/coin/service/TraderService.java
@@ -195,8 +195,8 @@ public class TraderService {
                     futures.getAsks().add(new Position(askProfit, askPrice));
 
                     if (sellAmount + i < balancing){
-                        buyAmount = (int) ((buyAmount + sellAmount)*0.8 + i);
-                        sellAmount = (int) ((buyAmount + sellAmount)*0.2 - i);
+                        buyAmount = (int) ((buyAmount + sellAmount)*0.2 + i);
+                        sellAmount = (int) ((buyAmount + sellAmount)*0.8 - i);
                     }
                 }
 
