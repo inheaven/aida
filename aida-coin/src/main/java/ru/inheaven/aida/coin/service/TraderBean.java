b5b3d0e336d0524dad00c5e6ffccc3c2dff52edc
Anatoly Ivanov <java@inhell.ru>
2015-01-25 15:51:15 +0600

    set 12 hour volatility

--- aida-coin/src/main/java/ru/inheaven/aida/coin/service/TraderBean.java
+++ aida-coin/src/main/java/ru/inheaven/aida/coin/service/TraderBean.java
@@ -87,7 +87,7 @@ public class TraderBean {
     public BigDecimal getSigma(ExchangePair exchangePair){
         try {
             return new BigDecimal((Double) em.createNativeQuery("select std(price) from ticker_history " +
-                    "where exchangetype = ? and pair = ? and `date` >  DATE_SUB(NOW(), INTERVAL 1 HOUR)")
+                    "where exchangetype = ? and pair = ? and `date` >  DATE_SUB(NOW(), INTERVAL 12 HOUR)")
                     .setParameter(1, exchangePair.getExchangeType().name())
                     .setParameter(2, exchangePair.getPair())
                     .getSingleResult());
