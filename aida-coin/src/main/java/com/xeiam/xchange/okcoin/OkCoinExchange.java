3e152c7045cecbbbc722ff020b5e59c8f3a3cd2b
Anatoly Ivanov <java@inhell.ru>
2015-02-09 21:58:38 +0600

    update xchange version

--- aida-coin/src/main/java/com/xeiam/xchange/okcoin/OkCoinExchange.java
+++ aida-coin/src/main/java/com/xeiam/xchange/okcoin/OkCoinExchange.java
@@ -13,25 +13,23 @@ import java.util.List;
 
 public class OkCoinExchange extends BaseExchange {
 
-  /**
-   * The parameter name of the symbols that will focus on.
-   */
-  public static final String SYMBOLS_PARAMETER = "symbols";
-  public static final String INTL_SYMBOLS_PARAMETER = "intl_symbols";
+    /**
+     * The parameter name of the symbols that will focus on.
+     */
 
-  private static final List<CurrencyPair> SYMBOLS = Arrays.asList(CurrencyPair.BTC_CNY, CurrencyPair.LTC_CNY);
-  private static final List<CurrencyPair> INTL_SYMBOLS = Arrays.asList(CurrencyPair.BTC_USD, CurrencyPair.LTC_USD);
+    private static final List<CurrencyPair> SYMBOLS = Arrays.asList(CurrencyPair.BTC_CNY, CurrencyPair.LTC_CNY);
+    private static final List<CurrencyPair> INTL_SYMBOLS = Arrays.asList(CurrencyPair.BTC_USD, CurrencyPair.LTC_USD);
 
-  @Override
-  public void applySpecification(ExchangeSpecification exchangeSpecification) {
-    super.applySpecification(exchangeSpecification);
+    @Override
+    public void applySpecification(ExchangeSpecification exchangeSpecification) {
+        super.applySpecification(exchangeSpecification);
 
-    this.pollingMarketDataService = new OkCoinMarketDataService(this);
-    if (exchangeSpecification.getApiKey() != null) {
-      this.pollingAccountService = new OkCoinAccountService(this);
-      this.pollingTradeService = new OkCoinTradeService(this);
+        this.pollingMarketDataService = new OkCoinMarketDataService(this);
+        if (exchangeSpecification.getApiKey() != null) {
+            this.pollingAccountService = new OkCoinAccountService(this);
+            this.pollingTradeService = new OkCoinTradeService(this);
+        }
     }
-  }
 
     @Override
     public SynchronizedValueFactory<Long> getNonceFactory() {
@@ -39,25 +37,37 @@ public class OkCoinExchange extends BaseExchange {
     }
 
     /**
-   * {@inheritDoc}
-   */
-  @Override
-  public ExchangeSpecification getDefaultExchangeSpecification() {
+     * {@inheritDoc}
+     */
+    @Override
+    public ExchangeSpecification getDefaultExchangeSpecification() {
 
-    ExchangeSpecification exchangeSpecification = new ExchangeSpecification(this.getClass().getCanonicalName());
-    exchangeSpecification.setSslUri("https://www.okcoin.cn/api");
-    exchangeSpecification.setHost("www.okcoin.cn");
-    exchangeSpecification.setExchangeName("OKCoin");
-    exchangeSpecification.setExchangeDescription("OKCoin is a globally oriented crypto-currency trading platform.");
-    exchangeSpecification.setExchangeSpecificParametersItem(SYMBOLS_PARAMETER, SYMBOLS);
-    exchangeSpecification.setExchangeSpecificParametersItem(INTL_SYMBOLS_PARAMETER, INTL_SYMBOLS);
+        ExchangeSpecification exchangeSpecification = new ExchangeSpecification(this.getClass().getCanonicalName());
+        exchangeSpecification.setSslUri("https://www.okcoin.cn/api");
+        exchangeSpecification.setHost("www.okcoin.cn");
+        exchangeSpecification.setExchangeName("OKCoin");
+        exchangeSpecification.setExchangeDescription("OKCoin is a globally oriented crypto-currency trading platform.");
 
-    exchangeSpecification.setExchangeSpecificParametersItem("Intl_SslUri", "https://www.okcoin.com/api");
-    exchangeSpecification.setExchangeSpecificParametersItem("Intl_Host", "www.okcoin.com");
+        exchangeSpecification.setExchangeSpecificParametersItem("Intl_SslUri", "https://www.okcoin.com/api");
+        exchangeSpecification.setExchangeSpecificParametersItem("Intl_Host", "www.okcoin.com");
 
-    // set to true to automatically use the Intl_ parameters for ssluri and host
-    exchangeSpecification.setExchangeSpecificParametersItem("Use_Intl", false);
-    return exchangeSpecification;
-  }
+        // set to true to automatically use the Intl_ parameters for ssluri and host
+        exchangeSpecification.setExchangeSpecificParametersItem("Use_Intl", true);
+
+        getMetaData().setCurrencyPairs(INTL_SYMBOLS);
+
+        return exchangeSpecification;
+    }
+
+    @Override
+    public String getMetaDataFileName(ExchangeSpecification exchangeSpecification) {
+
+        if (exchangeSpecification.getExchangeSpecificParametersItem("Use_Intl").equals(false)) {
+            return exchangeSpecification.getExchangeName().toLowerCase().replace(" ", "").replace("-", "").replace(".", "") + "_china";
+        } else {
+            return exchangeSpecification.getExchangeName().toLowerCase().replace(" ", "").replace("-", "").replace(".", "") + "_intl";
+
+        }
+    }
 
 }
