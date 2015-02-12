36bfef35b383d4a84fba0f084dd045bcdc6e2925
Anatoly Ivanov <java@inhell.ru>
2015-02-13 02:58:06 +0600

    add enum converters

--- aida-coin/src/main/java/ru/inheaven/aida/coin/entity/OrderTypeConverter.java
+++ aida-coin/src/main/java/ru/inheaven/aida/coin/entity/OrderTypeConverter.java
@@ -1,10 +1,12 @@
 package ru.inheaven.aida.coin.entity;
 
 import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
 
 /**
  * @author inheaven on 13.02.2015 3:31.
  */
+@Converter(autoApply = true)
 public class OrderTypeConverter implements AttributeConverter<OrderType, String> {
     @Override
     public String convertToDatabaseColumn(OrderType orderType) {
--- aida-coin/src/main/java/ru/inheaven/aida/coin/entity/TraderTypeConverter.java
+++ aida-coin/src/main/java/ru/inheaven/aida/coin/entity/TraderTypeConverter.java
@@ -1,10 +1,12 @@
 package ru.inheaven.aida.coin.entity;
 
 import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
 
 /**
  * @author inheaven on 13.02.2015 3:27.
  */
+@Converter(autoApply = true)
 public class TraderTypeConverter implements AttributeConverter<TraderType, String> {
     @Override
     public String convertToDatabaseColumn(TraderType traderType) {
