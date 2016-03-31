bb159130397ce7a0cf1afef5497e819725428061
Anatoly Ivanov <java@inhell.ru>
2016-03-31 23:23:20 +0600

    fix ltc

--- aida-happy-trading/src/main/java/ru/inheaven/aida/happy/trading/service/OkcoinCnFixService.java
+++ aida-happy-trading/src/main/java/ru/inheaven/aida/happy/trading/service/OkcoinCnFixService.java
@@ -3,7 +3,7 @@ package ru.inheaven.aida.happy.trading.service;
 import ru.inheaven.aida.happy.trading.entity.ExchangeType;
 
 import javax.inject.Singleton;
-import java.util.Collections;
+import java.util.Arrays;
 
 /**
  * @author inheaven on 04.09.2015 2:37.
@@ -12,7 +12,7 @@ import java.util.Collections;
 public class OkcoinCnFixService extends BaseOkcoinFixService {
     public OkcoinCnFixService() {
         super(ExchangeType.OKCOIN_CN, "a8c57680-52bb-4285-bf65-70b1d42f12b0", "81E1188B3084682F497DEF3B3EF9F740",
-                "client_market_cn.cfg", null, null, null, null, Collections.singletonList("BTC/CNY"));
+                "client_market_cn.cfg", null, null, null, null, Arrays.asList("LTC/CNY", "BTC/CNY"));
     }
 }
 
