cca28e4adb8757d1709cbaea169dbd563079c37c
Anatoly A. Ivanov <java@inheaven.ru>
2020-04-23 16:48:21 +0700

    add simple yocto color

--- aida/aida-color/bin/aida-color.cmd
+++ aida/aida-color/bin/aida-color.cmd
@@ -1,1 +1,2 @@
-powershell -command "& {$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'}"; "& {java -cp 'lib/*;*' ru.aida.color.Yocto | tee -Append aida-color.log}"
+rem powershell -command "& {$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'}"; "& {java -cp 'lib/*;*' ru.inheaven.aida.color.YoctoRandom | tee -Append aida-color.log}"
+java -classpath 'lib/*' ru.inheaven.aida.color.YoctoRandom
