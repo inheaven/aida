267eb350386f3d68be8e38281080744243b3c236
Anatoly A. Ivanov <java@inheaven.ru>
2020-04-29 13:38:58 +0700

    update random

--- aida-simple-color/src/main/java/ru/inheaven/aida/color/YoctoRandom.java
+++ aida-simple-color/src/main/java/ru/inheaven/aida/color/YoctoRandom.java
@@ -4,9 +4,11 @@ import com.yoctopuce.YoctoAPI.YAPI;
 import com.yoctopuce.YoctoAPI.YAPI_Exception;
 import com.yoctopuce.YoctoAPI.YColorLed;
 
-import java.security.SecureRandom;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -19,35 +21,47 @@ public class YoctoRandom {
         try {
             YAPI.RegisterHub("127.0.0.1");
 
-            Random random = new SecureRandom();
-
             YColorLed led1 = YColorLed.FindColorLed("YRGBLED1-018C9.colorLed1");
             YColorLed led2 = YColorLed.FindColorLed("YRGBLED1-018C9.colorLed2");
 
             Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
                 LocalDateTime localDateTime = LocalDateTime.now();
 
-                if (localDateTime.getMinute() % 10 != 0 || localDateTime.getSecond() != 0){
-                    return;
-                }
+                if ((localDateTime.getMinute() + 1) % 10 == 0 && localDateTime.getSecond() == 59){
+                    int c = localDateTime.getHour() > 8 && localDateTime.getHour() < 23 ? 256 : 32;
 
-                int c = localDateTime.getHour() > 8 && localDateTime.getHour() < 23 ? 256 : 32;
+                    int r = nextInt(c);
+                    int g = nextInt(c);
+                    int b = nextInt(c);
 
-                int r = random.nextInt(c);
-                int g = random.nextInt(c);
-                int b = random.nextInt(c);
+                    try {
+                        int rgbColor = ((r & 0x0ff) << 16) | ((g & 0x0ff) << 8) | (b & 0x0ff);
 
-                try {
-                    int rgbColor = ((r & 0x0ff) << 16) | ((g & 0x0ff) << 8) | (b & 0x0ff);
-
-                    led1.setRgbColor(rgbColor);
-                    led2.setRgbColor(rgbColor);
-                } catch (YAPI_Exception e) {
-                    e.printStackTrace();
+                        led1.setRgbColor(rgbColor);
+                        led2.setRgbColor(rgbColor);
+                    } catch (YAPI_Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }, 0, 1, TimeUnit.SECONDS);
         } catch (YAPI_Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static int nextInt(int bound){
+        try {
+            URL url = new URL("https://www.random.org/integers/?num=1&min=0&col=1&base=10&format=plain&rnd=new&max=" + bound);
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))){
+                return Integer.parseInt(in.readLine());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0;
     }
 }
