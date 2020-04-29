6eff59aece60d6babc38e5982494159e98e5c535
Anatoly A. Ivanov <java@inheaven.ru>
2020-04-29 13:39:32 +0700

    update random

--- aida-simple-color/src/main/java/ru/inheaven/aida/color/YoctoRandom.java
+++ aida-simple-color/src/main/java/ru/inheaven/aida/color/YoctoRandom.java
@@ -6,7 +6,6 @@ import com.yoctopuce.YoctoAPI.YColorLed;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.concurrent.Executors;
@@ -53,9 +52,7 @@ public class YoctoRandom {
         try {
             URL url = new URL("https://www.random.org/integers/?num=1&min=0&col=1&base=10&format=plain&rnd=new&max=" + bound);
 
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))){
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()))){
                 return Integer.parseInt(in.readLine());
             }
         } catch (Exception e) {
