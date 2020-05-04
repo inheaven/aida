e8b037ab25865d0f7d06125b9ff3eb2b521bf1b3
Anatoly A. Ivanov <java@inheaven.ru>
2020-05-04 18:00:22 +0700

    add random basic api

--- aida-simple-color/src/main/java/ru/inheaven/aida/color/YoctoRandom.java
+++ aida-simple-color/src/main/java/ru/inheaven/aida/color/YoctoRandom.java
@@ -3,11 +3,11 @@ package ru.inheaven.aida.color;
 import com.yoctopuce.YoctoAPI.YAPI;
 import com.yoctopuce.YoctoAPI.YAPI_Exception;
 import com.yoctopuce.YoctoAPI.YColorLed;
+import okhttp3.*;
+import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +16,11 @@ import java.util.concurrent.TimeUnit;
  * 23.04.2020 1:58 PM
  */
 public class YoctoRandom {
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    private static final String URL = "https://api.random.org/json-rpc/2/invoke";
+
+    private static final OkHttpClient CLIENT = new OkHttpClient();
+
     public static void main(String[] args) {
         try {
             YAPI.RegisterHub("127.0.0.1");
@@ -27,13 +32,13 @@ public class YoctoRandom {
                 LocalDateTime localDateTime = LocalDateTime.now();
 
                 if ((localDateTime.getMinute() + 1) % 10 == 0 && localDateTime.getSecond() == 59){
-                    int c = localDateTime.getHour() > 8 && localDateTime.getHour() < 23 ? 256 : 32;
-
-                    int r = nextInt(c);
-                    int g = nextInt(c);
-                    int b = nextInt(c);
+                    int c = localDateTime.getHour() > 8 && localDateTime.getHour() < 23 ? 255 : 32;
 
                     try {
+                        int r = nextInt(c);
+                        int g = nextInt(c);
+                        int b = nextInt(c);
+
                         int rgbColor = ((r & 0x0ff) << 16) | ((g & 0x0ff) << 8) | (b & 0x0ff);
 
                         led1.setRgbColor(rgbColor);
@@ -50,11 +55,39 @@ public class YoctoRandom {
 
     private static int nextInt(int bound){
         try {
-            URL url = new URL("https://www.random.org/integers/?num=1&min=0&col=1&base=10&format=plain&rnd=new&max=" + bound);
+            JSONObject json = new JSONObject();
 
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()))){
-                return Integer.parseInt(in.readLine());
+            json.put("jsonrpc", "2.0");
+            json.put("method","generateIntegers");
+
+            JSONObject params = new JSONObject();
+
+            params.put("apiKey", "8e9097d6-6a82-4234-a48e-d94577046b78");
+            params.put("n", 1);
+            params.put("min", 1);
+            params.put("max", bound);
+
+            json.put("params", params);
+
+            Long id = System.nanoTime();
+
+            json.put("id", id);
+
+            Request request = new Request.Builder()
+                    .url(URL)
+                    .post(RequestBody.create(json.toString(), JSON))
+                    .build();
+
+            try (Response response = CLIENT.newCall(request).execute()) {
+                JSONObject res = new JSONObject(Objects.requireNonNull(response.body()).string());
+
+                if (res.getLong("id") != id){
+                    throw new RuntimeException(id + " " + res);
+                }
+
+                return res.getJSONObject("result").getJSONObject("random").getJSONArray("data").getInt(0);
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
